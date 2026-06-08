8ac7bb064a66d4bf1226d9088d06ca7c93dd6a97
mekeirish <mmm1747lb@gmail.com>
2026-06-08 23:58:04 +0200

    Sniper Core Update

--- sw.js
+++ sw.js
@@ -1,13 +1,48 @@
-const cacheName = 'Laicheholding-v1';
-self.addEventListener('install', e => {
-  self.skipWaiting(); // Force l'activation immédiate
-  e.waitUntil(caches.open(cacheName).then(c => c.addAll(['./index.html', './manifest.json', './icon.png'])));
+const CACHE_NAME = 'markethub-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim()); // Prend le contrôle des pages immédiatement
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const url = new URL(event.request.url);
+        if (url.origin === self.location.origin) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(cachedResponse => cachedResponse || caches.match('./index.html'));
+      })
+  );
 });
