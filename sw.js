7cbae72782e41092644bd90b6fb2ba551b772b10
mekeirish <mmm1747lb@gmail.com>
2026-06-08 23:45:52 +0200

    Sniper Core Update

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const cacheName='Laicheholding-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const cacheName = 'Laicheholding-v1';
+self.addEventListener('install', e => {
+  self.skipWaiting(); // Force l'activation immédiate
+  e.waitUntil(caches.open(cacheName).then(c => c.addAll(['./index.html', './manifest.json', './icon.png'])));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim()); // Prend le contrôle des pages immédiatement
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
